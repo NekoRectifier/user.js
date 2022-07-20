cacf6ee0d10e820445fde7410149898b9c42131e
NekoRectifier <hyx123250@gmail.com>
2022-07-20 14:50:42 +0800

    Update 超星网盘直链下载.user.js

--- 超星网盘直链下载.user.js
+++ 超星网盘直链下载.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         超星网盘直链生成 - Optimized
 // @namespace    NEKO_CXNDDL
-// @version      1.1.1
+// @version      1.2
 // @description  添加更多功能
 // @author       NekoRectifier
 // @match        https://pan-yz.chaoxing.com/
+// @match        https://i.chaoxing.com/base?t=*
+// @match        https://pan-yz.chaoxing.com/external/m/file/*
 // @license      MIT
 // @require      https://cdn.bootcss.com/clipboard.js/1.5.16/clipboard.min.js
 // @require      https://unpkg.com/mdui@1.0.2/dist/js/mdui.min.js
@@ -18,16 +20,13 @@ var aria_method = "POST"
 
 //=============== CUSTOM VARIABLES ================
 
-var _item_num = 0;
-var succeededFilenames;
-
 function log(content) {
     console.log(content);
 }
 
 function copyToClipboard(str) {
-    document.getElementById("direct_download").setAttribute("data-clipboard-text", str)
-    var clipboard = new Clipboard("#direct_download");
+    $(".cdd_download")[0].setAttribute("data-clipboard-text", str)
+    var clipboard = new Clipboard(".cdd_download");
     clipboard.on('success', function (e) {
         console.log('复制成功！');
     });
@@ -37,18 +36,17 @@ function copyToClipboard(str) {
     });
 }
 
-function download_aria(url, name){
-    //url is single str now!
-    var req = 
+function download_aria(url, name) {
+    var req =
     {
-        id:'',
+        id: '',
         jsonrpc: '2.0',
         method: 'aria2.addUri',
         params: [
             [url],
             {
                 out: name,
-                header : [
+                header: [
                     'referer: https://i.chaoxing.com'
                 ]
             }
@@ -62,14 +60,12 @@ function download_aria(url, name){
         processData: false,
         data: JSON.stringify(req),
         contentType: 'application/json',
-        
-        success: function(res) {
-            console.log(res);
-        },
-        error: function(e) {
-            console.info(e.status);
-            console.info(e.readyState);
 
+        success: function (msg) {
+            console.log(msg);
+        },
+        error: function (e) {
+            // maybe there's other types of errors, but they cannot be tested so far... 
             if (e.status == 0) {
                 alert('Aria2 服务端未启动');
             } else {
@@ -79,99 +75,120 @@ function download_aria(url, name){
     })
 }
 
-(function () {
+function createButton(type) {
+    var download_btn = document.createElement("button");
+    download_btn.innerText = "下载 / 复制";
+    download_btn.setAttribute("class", "mdui-btn cdd_download");
+    download_btn.setAttribute("data-clipboard-text", "");
+    download_btn.setAttribute('data-clipboard-action', 'copy');
+
+    if (type == 1) {
+        // external version 
+        download_btn.setAttribute("id", "ex_copy_btn");
+        download_btn.setAttribute("style",
+            "margin-left:8px; background-color: #4CAF50; position: absolute; left:35%; margin-top:16px"
+        );
+    } else {
+        download_btn.setAttribute("id", "direct_download");
+        download_btn.setAttribute("onclick", "res.copy_direct_url();");
+        download_btn.setAttribute("style", "margin-left:8px; background-color: #4CAF50; float: center");
+    }
+
+    return download_btn;
+}
+
+function accquire_actual_url(filenode) {
+    var failedFilenames = new Array();
+    var failedUrlsAmount = 0;
+    var succeededUrls = new Array();
+    var succeededFilenames = new Array();
+    var succeededUrlsAmount = 0;
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open(
+        "get",
+        "https://pan-yz.chaoxing.com/external/m/file/" + filenode['id'],
+        false
+    );
+    xmlHttp.send();
+
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        succeededUrls[succeededUrlsAmount] =
+            xmlHttp.responseText.match(/https:\/\/d0.*(?=')/)[0].replace(/(?<=fn=).*/, filenode["name"]);
+        succeededFilenames[succeededUrlsAmount] = filenode["name"];
+        succeededUrlsAmount++;
+    } else {
+        failedFilenames[failedUrlsAmount] = filenode["name"];
+        failedUrlsAmount = failedUrlsAmount + 1;
+    }
+
+    if (succeededUrlsAmount > 0) {
+
+        if (succeededUrlsAmount > 1) {
+            //multi file downloads
+            for (var i = 0; i < succeededUrlsAmount; i++) {
+                download_aria(succeededUrls[i], succeededFilenames[i])
+            }
+        } else {
+            //single file
+            download_aria(succeededUrls[0], succeededFilenames[0])
+        }
+
+        alert('链接已复制；\nAria2 下载已开始');
+        copyToClipboard(succeededUrls);
+    }
+    if (failedUrlsAmount > 0) {
+        alert(
+            failedFilenames.toString() + " 等文件请求直链失败！（不支持文件夹）"
+        );
+        //TODO more friendiler failed prompt
+    }
+
+    return succeededUrls, succeededFilenames;
+}
+
+
+//main entry
+(function () { 
     "use strict";
     console.clear();
-
     $("head").append($(`<link rel="stylesheet" href="https://unpkg.com/mdui@1.0.2/dist/css/mdui.min.css" />`));
 
     var div = document.getElementsByClassName("ypActionBar")[0];
-    if (div) {
-        var download_btn = document.createElement("button");
-        download_btn.innerText = "下载 / 复制";
-        download_btn.setAttribute("class", "mdui-btn");
-        download_btn.setAttribute("style", "margin-left:8px; background-color: #4CAF50;")
-        download_btn.setAttribute("id", "direct_download");
-        download_btn.setAttribute("onclick", "res.copy_direct_url();");
-        download_btn.setAttribute("data-clipboard-text", "");
-        download_btn.setAttribute('data-clipboard-action', 'copy');
+    var href = window.location.href;
 
-        div.append(download_btn);
-        
+    if (div) {
+        div.append(createButton(0));
+
         res.copy_direct_url = function () {
 
-            function createXmlHttpRequest() {
-                try {
-                    return new XMLHttpRequest();
-                } catch (e) {
-                    return new ActiveXObject("Microsoft.XMLHTTP");
-                }
-            }
-
             if (res.choosedlen > 0) {
-                var failedFilenames = new Array();
-                var failedUrlsAmount = 0;
-                var succeededUrls = new Array();
-                succeededFilenames = new Array();
-                var succeededUrlsAmount = 0;
-
                 for (var filenode in res.choosed) {
-                    var xmlHttp = createXmlHttpRequest();
-                    xmlHttp.open(
-                        "get",
-                        "https://pan-yz.chaoxing.com/external/m/file/" + filenode,
-                        false
-                    );
-                    xmlHttp.send();
-                    
-                    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-                        var html_source = xmlHttp.responseText;
-                        var download_url = html_source.match(/https:\/\/d0.*(?=')/)[0];
-                        var filename = res.choosed[filenode]["name"];
-                        download_url = download_url.replace(/(?<=fn=).*/, filename);
-                        succeededUrls[succeededUrlsAmount] = download_url;
-                        succeededFilenames[succeededUrlsAmount] = filename;
-                        succeededUrlsAmount++;
-                    } else {
-                        failedFilenames[failedUrlsAmount] = res.choosed[filenode]["name"];
-                        failedUrlsAmount = failedUrlsAmount + 1;
-                    }
-                }
-
-
-                if (succeededUrlsAmount > 0) {
-                
-                    if (succeededUrlsAmount > 1) {
-                        //multi file downloads
-                        for (var i = 0; i < succeededUrlsAmount; i++) {
-                            download_aria(succeededUrls[i], succeededFilenames[i])
-                        }
-                    } else {
-                        //single file
-                        download_aria(succeededUrls[0], succeededFilenames[0])
-                    }
-
-                    alert('链接已复制；\nAria2 下载已开始');
-                    copyToClipboard(succeededUrls);
-                }
-                if (failedUrlsAmount > 0) {
-                    alert(
-                        failedFilenames.toString() + " 等文件请求直链失败！（不支持文件夹）"
-                    );
-                    //TODO more friendiler failed prompt
+                    accquire_actual_url(res.choosed[filenode])
                 }
             } else {
                 alert('请选中文件后再点击下载按钮');
             }
         };
 
-        mdui.mutation();
+    } else if (href.indexOf("pan-yz.chaoxing.com/external") != -1) {
+        log("currently in external sharing page");
 
+        $(".wid500").append(createButton(1));
+
+        $("#ex_copy_btn").click(function(event) {
+            var file_node_obj =
+            {
+                id: href.substring(href.lastIndexOf('/') + 1),
+                name: $("#name").text()
+            }
+
+            accquire_actual_url(file_node_obj);
+        });
+        
     } else {
         console.log("没有抓取到div");
     }
 })();
 
-$(document).ready(function () {
-
-})
+$(document).ready(function () { })
